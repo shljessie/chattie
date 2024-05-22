ed169e1ef377a536d90f46c9b0d5e23539b8b172
shljessie <sl994@cornell.edu>
2024-05-21 22:06:14 -0700

    update package

--- my-backend/index.js
+++ my-backend/index.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const csv = require('csv-parser');
 const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.use(cors());
 

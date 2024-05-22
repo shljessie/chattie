b9738ba9e38003e25d53400628e319ca9edd7f75
shljessie <sl994@cornell.edu>
2024-05-21 22:27:36 -0700

    Update heroku-postbuild script and correct path for static files

--- my-backend/index.js
+++ my-backend/index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3001;
 app.use(cors());
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '..', 'build')));
 
 const baseUrl = 'https://chattiedata.s3.amazonaws.com';
 
@@ -46,10 +46,9 @@ app.get('/load-csv', async (req, res) => {
   }
 });
 
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
+// The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
 });
 
 app.listen(port, (err) => {
